874fb416f67fc300d5597002235c8e9639603eec
Mairik4090 <119091217+Mairik4090@users.noreply.github.com>
2025-06-12 12:21:16 +0200

    Add default layout and about page

--- ki-stammbaum/pages/index.vue
+++ ki-stammbaum/pages/index.vue
@@ -8,6 +8,7 @@
 </template>
 
 <script setup lang="ts">
+definePageMeta({ layout: 'default' });
   import { useStammbaumData } from '@/composables/useStammbaumData';
 
   const { data: treeData, pending, error } = useStammbaumData();
